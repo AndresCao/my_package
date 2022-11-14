a195b348698301904201d4e82752cb2270d90bb8
Andres Cao <andrescao66@gmail.com>
2022-11-14 15:29:16 -0500

    fourth commit

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -1,11 +1,12 @@
 from http.server import executable
 from importlib.resources import Package
 from struct import pack
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LanchDescription([
+    return LaunchDescription([
         Node(
             package='demo_nodes_cpp',
             executable='listener'
--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LanchDescription([
+    return LaunchDescription([
         Node(
             package='demo_nodes_cpp',
             executable='talker'
